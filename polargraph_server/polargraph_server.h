717f888f3608596c990c8bd283fbadf10e18c81a
Matteo Geromin <geromin.matteo@gmail.com>
2016-06-03 15:53:08 +0200

    Add new circle drawing functions. Remove old ones

--- polargraph_server/polargraph_server.h
+++ polargraph_server/polargraph_server.h
@@ -257,6 +257,7 @@ void testPenWidth();						// C11
 void testPenWidthScribble();				// C12
 void penDown();								// C13
 void penUp();								// C14
+void curves_pixel_drawCircularPixel();		// C16
 void changeLengthDirect();					// C17
 void setMachineSizeFromCommand();			// C24
 void setMachineNameFromCommand();			// C25
@@ -357,6 +358,7 @@ void runToPosition(unsigned char side);
 void engageMotors();
 void runToNewPosition(long position , unsigned char side);
 void releaseMotors();
+void curves_drawSpiral(long centerx, long centery, int maxRadius, int increment, int density);
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 //  FUNCTIONS
@@ -2480,64 +2482,183 @@ float rads(int n) {
 	return (n/180.0 * PI);
 }
 
-void drawCurve(float x, float y, float fx, float fy, float cx, float cy) {
-	// Draw a Quadratic Bezier curve from (x, y) to (fx, fy) using control pt
-	// (cx, cy)
-	float xt=0;
-	float yt=0;
-	float t;
+void curves_pixel_drawCircularPixel() 
+{
+    long originA = asLong(inParam1);
+    long originB = asLong(inParam2);
+    int size = asInt(inParam3);
+    int density = asInt(inParam4);
 
-	for (t=0; t<=1; t+=.0025) {
-		xt = pow((1-t),2) *x + 2*t*(1-t)*cx+ pow(t,2)*fx;
-		yt = pow((1-t),2) *y + 2*t*(1-t)*cy+ pow(t,2)*fy;
-		changeLength_float(xt, yt);
-	}
+    int radius = size / 2;
+
+    //Serial.print("Density before: ");
+    //Serial.println(density);
+    
+    int maxDensity_var = maxDensity(penWidth, radius);
+    //Serial.print("Max density: " );
+    //Serial.print(maxDensity);
+    //Serial.print(", penWidth: ");
+    //Serial.print(penWidth);
+    //Serial.print(", radius: ");
+    //Serial.println(radius);
+     
+    density = scaleDensity(density, 255, maxDensity_var);
+    //Serial.print("Density scaled");
+    //Serial.println(density);
+    
+    int increment = radius / maxDensity_var;
+
+    if (density > 0)
+    {
+      //Serial.print("Density:");
+      //Serial.println(density);
+      curves_drawSpiral(originA, originB, radius, increment, density);
+    }
+    
+}
+void curves_drawCurve(long x, long y, long fx, long fy, long cx, long cy, int speedOfSegment) {
+  // Draw a Quadratic Bezier curve from (x, y) to (fx, fy) using control pt
+  // (cx, cy)
+  float xt=0;
+  float yt=0;
+  float t;
+
+  for (t=0; t<=1; t+=.01) {
+    xt = pow((1-t),2) *x + 2*t*(1-t)*cx+ pow(t,2)*fx;
+    yt = pow((1-t),2) *y + 2*t*(1-t)*cy+ pow(t,2)*fy;
+    
+    if (speedOfSegment != 0)
+    {
+      setSpeed(speedOfSegment,SX);
+      setSpeed(speedOfSegment,DX);
+      //usingAcceleration = false;
+    }
+    reportingPosition = false;
+    changeLength_float(xt, yt);
+    reportingPosition = true;
+    //usingAcceleration = true;
+  }  
 }
 
-void drawCircle(int centerx, int centery, int radius) {
-	// Estimate a circle using 20 arc Bezier curve segments
-	int segments = 20;
-	int angle1 = 0;
-	int midpoint = 0;
-	float angle2;
+float desiredSpeed_new(long distanceTo, float currentSpeed, float acceleration)
+{
+    float requiredSpeed;
 
-	changeLength_long(centerx+radius, centery);
+    if (distanceTo == 0)
+	return 0.0f; // We're there
 
-	for (angle2=360/segments; angle2<=360; angle2+=360/segments) {
+    // sqrSpeed is the signed square of currentSpeed.
+    float sqrSpeed = currentSpeed * currentSpeed;
+    if (currentSpeed < 0.0)
+      sqrSpeed = -sqrSpeed;
+      
+    float twoa = 2.0f * acceleration; // 2ag
+    
+    // if v^^2/2as is the the left of target, we will arrive at 0 speed too far -ve, need to accelerate clockwise
+    if ((sqrSpeed / twoa) < distanceTo)
+    {
+	// Accelerate clockwise
+	// Need to accelerate in clockwise direction
+	if (currentSpeed == 0.0f)
+	    requiredSpeed = sqrt(twoa);
+	else
+	    requiredSpeed = currentSpeed + fabs(acceleration / currentSpeed);
 
-		midpoint = angle1+(angle2-angle1)/2;
-
-		float startx=centerx+radius*cos(rads(angle1));
-		float starty=centery+radius*sin(rads(angle1));
-		float endx=centerx+radius*cos(rads(angle2));
-		float endy=centery+radius*sin(rads(angle2));
-		int t1 = rads(angle1)*1000 ;
-		int t2 = rads(angle2)*1000;
-		//int t3 = angle1;
-		//int t4 = angle2;
-
-		drawCurve(startx,starty,endx,endy,
-			centerx+2*(radius*cos(rads(midpoint))-.25*(radius*cos(rads(angle1)))-.25*(radius*cos(rads(angle2)))),
-			centery+2*(radius*sin(rads(midpoint))-.25*(radius*sin(rads(angle1)))-.25*(radius*sin(rads(angle2))))
-		);
-
-		angle1=angle2;
-	}
-
+	if (requiredSpeed > currentMaxSpeed)
+	    requiredSpeed = currentMaxSpeed;
+    }
+    else
+    {
+	// Decelerate clockwise, accelerate anticlockwise
+	// Need to accelerate in clockwise direction
+	if (currentSpeed == 0.0f)
+	    requiredSpeed = -sqrt(twoa);
+	else
+	    requiredSpeed = currentSpeed - fabs(acceleration / currentSpeed);
+	if (requiredSpeed < -currentMaxSpeed)
+	    requiredSpeed = -currentMaxSpeed;
+    }
+    
+    //Serial.println(requiredSpeed);
+    return requiredSpeed;
 }
 
-void drawCircles(int number, int centerx, int centery, int r) {
-	// Draw a certain number of concentric circles at the given center with
-	// radius r
-	int dr=0;
-	int k;
-	if (number > 0) {
-		dr = r/number;
-		for (k=0; k<number; k++) {
-			drawCircle(centerx, centery, r);
-			r=r-dr;
-		}
-	}
+void curves_drawSpiral(long centerx, long centery, int maxRadius, int increment, int density) 
+{
+  //Serial.println("Draw spiral.");
+  //Serial.print("Max radius: ");
+  //Serial.println(maxRadius);
+  //Serial.print("Increment:");
+  //Serial.println(increment);
+  //Serial.print("density:");
+  //Serial.println(density);
+  // Estimate a circle using 20 arc Bezier curve segments
+  int segments = 20;
+  int totalSegments = segments * density;
+
+  
+  float radius = (float)(increment);
+  
+  int turns;
+  // work out how many shells to draw
+  int runSpeed_var = 0;
+  for (turns = 0; turns < density; turns++)
+  {
+//    if (increment < 80)
+//      segments = radius / 4;
+//    if (segments < 5)
+//      segments = 5;
+      
+
+    float anglePerSegment = 360/segments;
+    
+    float radiusIncrementPerSegment = (float)(increment) / (float)(segments);
+  
+    int angle1 = 0;
+    int midpoint=0;
+    char firstMove = true;
+    float angle2;
+    for (angle2=anglePerSegment; angle2<=360; angle2+=anglePerSegment) 
+    {
+
+      midpoint = angle1+(angle2-angle1)/2;
+  
+      float startx=centerx+radius*cos(rads(angle1));
+      float starty=centery+radius*sin(rads(angle1));
+      float endx=centerx+radius*cos(rads(angle2));
+      float endy=centery+radius*sin(rads(angle2));
+      
+      int t1 = rads(angle1)*1000 ;
+      int t2 = rads(angle2)*1000;
+      //int t3 = angle1;
+      //int t4 = angle2;
+  
+      if (firstMove)
+      {
+        changeLength_float(startx, starty);
+        firstMove = false;
+      }
+      // do the move
+      runSpeed_var = desiredSpeed_new(totalSegments, runSpeed_var, currentAcceleration*2);
+//      Serial.print("segment: ");
+//      Serial.print(totalSegments);
+//      Serial.print(", runSpeed: ");
+//      Serial.println(runSpeed);
+      //usingAcceleration = false;
+      curves_drawCurve(startx,starty,endx,endy
+                ,centerx+2*(radius*cos(rads(midpoint))-.25*(radius*cos(rads(angle1)))-.25*(radius*cos(rads(angle2))))
+                ,centery+2*(radius*sin(rads(midpoint))-.25*(radius*sin(rads(angle1)))-.25*(radius*sin(rads(angle2))))
+                ,runSpeed_var
+      );
+      totalSegments--;
+      //usingAcceleration = true;
+      angle1=angle2;
+      radius += radiusIncrementPerSegment;
+
+    }
+//    Serial.println("Finished drawing turn.");
+  }
+  //Serial.println("Finished spiral pixel.");
 }
 
 long getCartesianX() {
